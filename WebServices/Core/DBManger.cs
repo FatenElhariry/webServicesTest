6e552532c485d27095b077d2f4132632d6d271d2
Faten Elhariry <aelhariry78@yahoo.com>
2018-08-17 18:20:02 +0200

    initial commit

---
+++ WebServices/Core/DBManger.cs
@@ -0,0 +1,61 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+    public  class DBManger
+    {
+        static string strcon;
+         static DBManger()
+        {
+            strcon = ConfigurationManager.ConnectionStrings["con"].ConnectionString;                 
+        }
+
+        public static int ExecuteNonQuery(string query, CommandType type, params SqlParameter[] arr)
+        {
+            int RowEffected = 0;
+
+            SqlConnection con = new SqlConnection(strcon);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.CommandType = type;
+            if (arr != null)
+                cmd.Parameters.AddRange(arr);
+             RowEffected= cmd.ExecuteNonQuery();
+         
+            con.Close();
+
+            return RowEffected;
+        }
+        public static object ExecuteScalor(string query,CommandType type,params SqlParameter[]arr)
+        {
+            object myreturn = 0;
+
+            SqlConnection con = new SqlConnection(strcon);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.CommandType = type;
+            if (arr.Length!=0)
+                cmd.Parameters.AddRange(arr);
+            
+            myreturn = cmd.ExecuteScalar();
+            con.Close();
+            return (int)myreturn;
+
+            
+        }
+        public static DataTable ExecuteDataTable(string query, CommandType type, params SqlParameter[] arr)
+        {
+            SqlConnection con = new SqlConnection(strcon);
+           // con.Open();
+            SqlCommand cmd = new SqlCommand(query, con);
+            cmd.CommandType = type;
+            if (arr.Length!=0)
+                cmd.Parameters.AddRange(arr);
+            SqlDataAdapter adpt = new SqlDataAdapter(cmd);
+
+            DataTable td = new DataTable();
+            adpt.Fill(td);
+           // con.Close();
+            return td;
+        }
+    }
---
+++ WebServices/test.asmx.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Web.Services;
+using WebServices.Core;
+using WebServices.Modal;
+
+namespace WebServices
+{
+    /// <summary>
+    /// Summary description for test
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class test : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public List<Product> GetALLProduct()
+        {
+          DataTable dt =  DBManger.ExecuteDataTable("select * from products ", 
+                     System.Data.CommandType.Text);
+           List<Product> products = Helper.DataTableToList<Product>(dt);
+            return products;
+        }
+    }
+}
